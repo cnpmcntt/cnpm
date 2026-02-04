9e895fab3d6ca78992f6e33baf9c4d52c7b4a91e
pthhanhh777-svg <pthhanhh777@gmail.com>
2026-02-05 01:48:04 +0700

    Update main.py

--- IGCSE Learning/main.py
+++ IGCSE Learning/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI, Request, Depends
 from fastapi.responses import RedirectResponse
-from fastapi.staticfiles import StaticFiles
 from database import engine
 import models
-from dependencies import get_current_user
-
-from routers import auth, admin, teacher, student
+from routers import auth, admin, manager, teacher, student, parent, profile
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -13,17 +10,12 @@ app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(admin.router)
+app.include_router(manager.router)
 app.include_router(teacher.router)
 app.include_router(student.router)
+app.include_router(parent.router)
+app.include_router(profile.router)
 
 @app.get("/")
-async def root(user: models.User = Depends(get_current_user)):
-    if not user:
-        return RedirectResponse("/login")
-    if user.role == "admin":
-        return RedirectResponse("/admin")
-    elif user.role == "teacher":
-        return RedirectResponse("/teacher")
-    elif user.role == "student":
-        return RedirectResponse("/student")
+async def root():
     return RedirectResponse("/login")
